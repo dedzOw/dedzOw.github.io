c504c3cd49fa893f1204f7fb74482b5b0e1ca456
Jonny Shank <jonnylshank@gmail.com>
2026-09-09 16:00:11 -0400

    adding 04js example from class

---
+++ csce242/exercises/04js/scripts/intro.js
@@ -0,0 +1,6 @@
+
+document.getElementById("btn-show-message").onclick = showMessage();
+
+document.getElementById("btn-show-message").onclick = () => {
+    console.log("I did it. Button Clicked");
+}
