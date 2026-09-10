0add56d77e54dfee86dc3d3fe9249197a39db2f9
Jonny Shank <jonnylshank@gmail.com>
2026-09-10 11:48:23 -0400

    04js code update

--- csce242/exercises/04js/scripts/intro.js
+++ csce242/exercises/04js/scripts/intro.js
@@ -1,6 +1,4 @@
-
-document.getElementById("btn-show-message").onclick = showMessage();
-
-document.getElementById("btn-show-message").onclick = () => {
-    console.log("I did it. Button Clicked");
-}
+document.getElementById("btn-show-message").onclick = (e) => {
+    document.getElementById("p-message").innerHTML = "Hello World";
+    e.target.innerHTML = "done!";
+};
